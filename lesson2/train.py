37945417dd8d0b1ad8b1b532141e74cf287ae1ae
Ali Ismail <47851607+ali-ism@users.noreply.github.com>
2022-08-10 16:56:08 +0300

    fixed epochs

--- lesson2/train.py
+++ lesson2/train.py
@@ -27,7 +27,6 @@ cfg.batch_size = 32
 cfg.lr = 0.0001
 cfg.arch = 'resnet'
 cfg.dropout_rate = 0.5
-cfg.epochs = 5
 cfg.fc_neurons = 128
 
 
@@ -141,7 +140,7 @@ def train(cfg):
     dataset, train_dataset, val_dataset, test_dataset = load_data(cfg)
     model = Model(cfg, dataset, train_dataset, val_dataset, test_dataset)
     trainer = pl.Trainer(
-        max_epochs=cfg.epochs,
+        max_epochs=10,
         accelerator='auto',
         deterministic=True,
         logger=wandb_logger)
